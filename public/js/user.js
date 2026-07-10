6cb60e807f2538a0daba8e89fbb44b7f72e3aa41
syrinemrf <syrine.mrf@gmail.com>
2026-07-10 13:00:50 +0100

    add: edit and list users

--- public/js/user.js
+++ public/js/user.js
@@ -8,44 +8,83 @@ $(document).ready(function () {
 
     });
 
+    $('#email').change(function () {
+
+        const emailInput = $(this);
+
+        $.ajax({
+            type: "POST",
+            url: emailInput.data('check-url'),
+            data: {
+                email: emailInput.val(),
+                id: emailInput.data('user-id')
+            },
+
+            success: function (response) {
+                response = response.trim();
+
+                if (response === 'email_exists') {
+                    alert("Cet email est déjà utilisé.");
+                    emailInput.val('');
+                }
+            }
+        });
+
+    });
+
+
 
     $('#user-form').submit(function (e) {
 
         e.preventDefault();
 
+        const form = $(this);
+        const submitButton = form.find('button[type="submit"]');
+
+        submitButton.prop('disabled', true);
+
         $.ajax({
             type: "POST",
-            url: "/users/add",
-            data: $(this).serialize(),
-            success: function () {
-                alert("Utilisateur ajouté avec succès.");
-                $('#user-form')[0].reset();
-                toggleLibelle();
+            url: this.action,
+            data: form.serialize(),
+
+            success: function (response) {
+                response = response.trim();
+
+                if (response === 'success') {
+                    alert(form.data('success-message'));
+                    if (form.data('reset-form')) {
+                        form[0].reset();
+                    }
+
+                    toggleLibelle();
+                }
             },
 
             error: function () {
-
                 alert("Une erreur est survenue.");
+            },
 
+            complete: function () {
+                submitButton.prop('disabled', false);
             }
-
         });
 
     });
 
-});
 
+    function toggleLibelle() {
 
-function toggleLibelle() {
+        if ($('input[name="role"]:checked').val() === "ROLE_FOURNISSEUR") {
 
-    if ($('input[name="role"]:checked').val() === "ROLE_FOURNISSEUR") {
+            $('#libelle-container').show();
 
-        $('#libelle-container').show();
+        } else {
 
-    } else {
+            $('#libelle-container').hide();
 
-        $('#libelle-container').hide();
+        }
 
     }
 
-}
+});
